8f3d98062171b8c2165796ad471cbcb584112ea5
epalle <epalle.cyrille@gmail.com>
2019-02-12 17:58:14 +0100

    add impl

--- src/expert_system/lexer.clj
+++ src/expert_system/lexer.clj
@@ -3,9 +3,6 @@
 
 (load "util")
 
-; TODO handel error
-
-
 (def tokens
   '(:eol
     :queries
@@ -30,14 +27,23 @@
     \^ :xor
     \? :queries))
 
+(defn get-token-multi-char [list-char next]
+  (let [frst-char (first list-char)
+        scnd-char (second list-char)]
+    (cond
+      (and (= frst-char \<) (= scnd-char \=)) (conj (next (rest (rest list-char))) :impl-left)
+      (and (= frst-char \=) (= scnd-char \>)) (conj (next (rest (rest list-char))) :impl-right)
+      (= frst-char \=)                        (conj (next (rest list-char)) :queries)
+      :else                                   (next (rest list-char)))))
+
 (defn list-char->tokens [idx list-char]
   (let [frst-char (first list-char)
         next      #(list-char->tokens idx %)]
     (cond
       (or (= frst-char nil) (= frst-char \#))                                      '(:eol)
-      (in? (seq "()!+|^?") frst-char)                                              (conj (next (rest list-char)) (choose-token-mono-char frst-char))
       (in? (seq "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") frst-char) (conj (next (rest list-char)) frst-char)
-      (in? (seq "=<>") frst-char)                                                  (conj (next (rest list-char)) "TODO")
+      (in? (seq "()!+|^?") frst-char)                                              (conj (next (rest list-char)) (choose-token-mono-char frst-char))
+      (in? (seq "=<>") frst-char)                                                  (get-token-multi-char list-char next)
       (in? (seq " \t\n") frst-char)                                                (next (rest list-char))
       :else                                                                        (next (rest list-char)))))
 

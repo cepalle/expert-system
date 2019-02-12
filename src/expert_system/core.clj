c15d93f726095928931fbb905f114a0316e7962b
epalle <epalle.cyrille@gmail.com>
2019-02-12 11:15:01 +0100

    open file

--- src/expert_system/core.clj
+++ src/expert_system/core.clj
@@ -1,4 +1,17 @@
 (ns expert-system.core)
 
+(defn Example [fileName]
+  (println (str "open: " fileName))
+  (slurp fileName))
+
+(defn ExampleLine [fileName]
+  (println (str "open: " fileName))
+  (with-open [rdr (clojure.java.io/reader fileName)]
+    (reduce conj [] (line-seq rdr))))
+
+(defn ExampleFileExist []
+  (println (.exists (clojure.java.io/file "Example.txt"))))
+
 (defn -main [& args]
-  (println "Hello, World!"))
+  (println (ExampleLine (first args))))
+

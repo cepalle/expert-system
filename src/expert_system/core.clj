3b8ad10cb00f3a6e977f50315ef6d2bbec25d7a0
epalle <epalle.cyrille@gmail.com>
2019-02-12 11:44:47 +0100

    add filter

--- src/expert_system/core.clj
+++ src/expert_system/core.clj
@@ -1,5 +1,10 @@
 (ns expert-system.core)
 
+(require
+ '[clojure.string :as str])
+
+; ----
+
 (defn Example [fileName]
   (println (str "open: " fileName))
   (slurp fileName))
@@ -15,8 +20,25 @@
 (defn MyPrint [s]
   (println (str "coucou" s "kookoo")))
 
+
+(defn LexerLine [line]
+  (str/split line #" "))
+
+(defn Lexer [lines]
+  (def splitLine (mapcat LexerLine lines))
+  (filter
+    (fn [x]
+      (not (= (count x) 0)))
+    splitLine))
+
+
+(defn MyPrintListe [list]
+  (doseq [item list]
+    (println item)))
+
 (defn -main [& args]
   (def res (ExampleLine (first args)))
-  (doseq [item res]
-    (println item)))
+  (MyPrintListe res)
+  (def resLexer (Lexer res))
+  (MyPrintListe resLexer))
 

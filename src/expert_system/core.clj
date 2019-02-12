89dd014376a83445dbc80a234d15b986d2585055
epalle <epalle.cyrille@gmail.com>
2019-02-12 12:11:45 +0100

    refacto

--- src/expert_system/core.clj
+++ src/expert_system/core.clj
@@ -3,42 +3,31 @@
 (require
  '[clojure.string :as str])
 
-; ----
 
-(defn Example [fileName]
-  (println (str "open: " fileName))
-  (slurp fileName))
-
-(defn ExampleLine [fileName]
+(defn open-file-lines [fileName]
   (println (str "open: " fileName))
   (with-open [rdr (clojure.java.io/reader fileName)]
     (reduce conj [] (line-seq rdr))))
 
-(defn ExampleFileExist []
+(defn file-exists? []
   (println (.exists (clojure.java.io/file "Example.txt"))))
 
-(defn MyPrint [s]
-  (println (str "coucou" s "kookoo")))
-
-
-(defn LexerLine [line]
+(defn line->tokens [line]
   (str/split line #" "))
 
-(defn Lexer [lines]
-  (def splitLine (mapcat LexerLine lines))
+(defn lines->tokens [lines]
+  (def splitLine (mapcat line->tokens lines))
   (filter
-    (fn [x]
-      (not (= (count x) 0)))
-    splitLine))
+   (fn [x] (not (= (count x) 0)))
+   splitLine))
 
 
-(defn MyPrintListe [list]
+(defn my-print-list [list]
   (doseq [item list]
     (println item)))
 
 (defn -main [& args]
-  (def res (ExampleLine (first args)))
-  (MyPrintListe res)
-  (def resLexer (Lexer res))
-  (MyPrintListe resLexer))
-
+  (def res (open-file-lines (first args)))
+  (my-print-list res)
+  (def resLexer (lines->tokens res))
+  (my-print-list resLexer))

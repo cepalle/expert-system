dbbac05dec12f80dff7ee3c25179027464795a9e
epalle <epalle.cyrille@gmail.com>
2019-02-12 17:12:41 +0100

    add key

--- src/expert_system/lexer.clj
+++ src/expert_system/lexer.clj
@@ -3,25 +3,43 @@
 
 (load "util")
 
-(defn del-com [line]
-  (first (str/split line #"#")))
-
-(defn del-space [line]
-  (filter (fn [x] (not (in? " \t\n" x))) line))
-
 ; TODO handel error
+
+
+(def tokens
+  '(:eol
+    :queries
+    :facts
+    :equival
+    :impl-left
+    :impl-right
+    :xor
+    :or
+    :and
+    :neg
+    :par-open
+    :par-close))
+
+(defn choose-token-mono-char [c]
+  (case c
+    \( :par-open
+    \) :par-close
+    \! :neg
+    \+ :and
+    \| :or
+    \^ :xor
+    \? :queries))
 
 (defn list-char->tokens [idx list-char]
   (let [frst-char (first list-char)
         next      #(list-char->tokens idx %)]
     (cond
-      (= frst-char nil)                                                                                             '("EOL")
-      (= \# frst-char)                                                                                              '("EOL")
-      (in? (seq " ") frst-char)                                                                                     (next (rest list-char))
-      (in? (seq "=<>") frst-char)                                                                                   (conj (next (rest list-char)) "TODO")
-      (in? (seq "()!+|^") frst-char)                                                                                (conj (next (rest list-char)) frst-char)
-      (in? (seq "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") frst-char)                                  (conj (next (rest list-char)) frst-char)
-      :else                                                                                                         (next (rest list-char)))))
+      (or (= frst-char nil) (= frst-char \#))                                      '(:eol)
+      (in? (seq "()!+|^?") frst-char)                                              (conj (next (rest list-char)) (choose-token-mono-char frst-char))
+      (in? (seq "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") frst-char) (conj (next (rest list-char)) frst-char)
+      (in? (seq "=<>") frst-char)                                                  (conj (next (rest list-char)) "TODO")
+      (in? (seq " \t\n") frst-char)                                                (next (rest list-char))
+      :else                                                                        (next (rest list-char)))))
 
 (defn line->tokens [[idx line]]
   (list-char->tokens idx (seq line)))

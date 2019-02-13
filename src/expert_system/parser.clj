6c7c6dfea8bfc2f46e90c936fb4af2d3372b8c33
epalle <epalle.cyrille@gmail.com>
2019-02-13 10:08:16 +0100

    :par

--- src/expert_system/parser.clj
+++ src/expert_system/parser.clj
@@ -6,8 +6,21 @@
   ([]))
 
 (defn parse-par
-  ([tokens] tokens)
-  ([]))
+  ([tokens]
+   (if (in? tokens :par-open)
+     (parse-par '() '() 0 tokens)
+     (parse-neg tokens)))
+  ([exp in-par nb-par-open tokens]
+   (let [frst (first tokens)
+         rst  (rest tokens)]
+     (cond
+       (= frst nil)                                (reverse exp)
+       (and (= frst :par-open) (= nb-par-open 0))  (parse-par exp in-par (inc nb-par-open) rst)
+       (and (= frst :par-open) (> nb-par-open 0))  (parse-par exp (conj in-par frst) (inc nb-par-open) rst)
+       (and (= frst :par-close) (= nb-par-open 1)) (parse-par (conj exp (list :par (parse-par (reverse in-par)))) '() (dec nb-par-open) rst)
+       (and (= frst :par-close) (> nb-par-open 1)) (parse-par exp (conj in-par frst) (dec nb-par-open) rst)
+       (> nb-par-open 0)                           (parse-par exp (conj in-par frst) nb-par-open rst)
+       :else                                       (parse-par (conj exp frst) in-par nb-par-open rst)))))
 
 (defn tokens->exp [tokens]
   (let [with-par (parse-par tokens)

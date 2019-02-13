b44497f1c884655e736c0f83e601fbdf8f745f00
epalle <epalle.cyrille@gmail.com>
2019-02-13 08:59:12 +0100

    try rec pars

--- src/expert_system/parser.clj
+++ src/expert_system/parser.clj
@@ -1,5 +1,39 @@
 (load "util")
 
+(defn graph-xor
+  ([tokens]
+   (if (some #(= :xor %) tokens)
+     ()
+     (graph-neg tokens)))
+  ([graph tokens]))
+
+(defn graph-or
+  ([tokens]
+   (if (some #(= :or %) tokens)
+     ()
+     (graph-xor tokens)))
+  ([graph tokens]))
+
+(defn graph-and
+  ([tokens]
+   (if (some #(= :and %) tokens)
+     ()
+     (graph-or tokens)))
+  ([graph tokens]))
+
+(defn graph-neg
+  ([tokens]
+   (if (some #(= :neg %) tokens)
+     ()
+     (graph-and tokens)))
+  ([graph tokens]))
+
+(defn graph-par
+  ([tokens]
+   (if (some #(= :par-open %) tokens)
+     (graph-par '(:par) tokens)
+     (graph-neg tokens)))
+  ([graph tokens]))
 
 (defn graph-eof
   ([tokens]

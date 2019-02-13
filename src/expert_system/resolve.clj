4c2bee66cfd4bd831fed9ff3bb2a1aca9af28a2b
epalle <epalle.cyrille@gmail.com>
2019-02-13 15:34:52 +0100

    get feild

--- src/expert_system/resolve.clj
+++ src/expert_system/resolve.clj
@@ -1,5 +1,17 @@
+(defn exp->feilds [exp]
+  (map
+   (fn [ep]
+     (cond
+       (list? ep) (exp->feilds ep)
+       (char? ep) ep
+       :else      nil))
+   exp))
+
+(defn epxs->feilds [exps] (filter #(not (= nil %)) (set (flatten (map exp->feilds exps)))))
+
 (defn resolve-grph [st-parser]
-  (let [queris (:queries st-parser)
-        facts  (:facts st-parser)
-        exps   (:exps st-parser)]
-    "coucou"))
+  (let [queris    (:queries st-parser)
+        facts     (:facts st-parser)
+        exps      (:exps st-parser)
+        all-field (epxs->feilds exps)]
+    all-field))

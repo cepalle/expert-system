8c87407bf1629b8e81a3ae8ccdaf7834b8d2fb5b
Guillaume <guillaume.granjondelepiney@laposte.net>
2019-02-19 22:29:51 +0100

    Beginning rework backward parser => now, have to simplify and if after implication

--- src/expert_system/backward_parser.clj
+++ src/expert_system/backward_parser.clj
@@ -1,32 +1,41 @@
+
+(defn exit-message [msg]
+  (println msg "If you want to activate bypass this error message, add '-f'")
+  (System/exit 0))
+
 (defn check-side-imp [exp side]
   (let [to-check (if (= side "right")
                    (last exp)
                    (nth exp 1))
         res (not (or (some #{:xor} (flatten to-check))
-                     (some #{:or} (flatten to-check))
-                     (some #{:and} (flatten to-check))))
+                     (some #{:or} (flatten to-check))))
         ]
     res))
 
-(defn imply-only-one [exp]
+(defn good-imply [exp]
   (let [expr (first exp)
         res     (cond
                   (= (first expr) :impl-right) (check-side-imp expr "right")
                   (= (first expr) :impl-left) (check-side-imp expr "left")
-                  (= (first expr) :equival) (or (check-side-imp expr "right") (check-side-imp expr "left"))
-                  :else true
+                  :else false
                   )]
     res))
 
 (defn check-all-imply [exps]
-  (every? identity (map imply-only-one (map vector exps))))
+  (every? identity (map good-imply (map vector exps))))
 
-(defn count-imp [str-exp]
-  (+ (count (re-seq #":equival" str-exp))
-     (count (re-seq #":impl" str-exp))))
+(defn imply-line [prop]
+  (= 1 (count (re-seq #":impl" (str prop)))))
 
-(defn check-imp [exps]
-  (= (count exps) (reduce + (for [prop (map vector exps)]
-                              (if (or (re-seq #":equival" (str prop)) (re-seq #":impl" (str prop)))
-                                (count-imp (str prop))
-                                1)))))
+(defn count-imp-line [exps]
+  (every? identity (map #(= 1 (count (re-seq #":impl" (str %)))) (map vector exps))))
+
+(defn contains-equiv [exps]
+  (re-seq #":equival" (str (list exps))))
+
+(defn can-be-resolve [exps]
+  (cond
+    (contains-equiv exps) (exit-message "Error, there is an equivalence.")
+    (not (count-imp-line exps)) (exit-message "Error, there are too much/is not enough implication(s) on the same line.")
+    (not (good-imply exps)) (exit-message "Error, check what's there after your implication.")
+    :else true))

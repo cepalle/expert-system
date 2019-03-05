946afb716eb21604627f3e6b77e4e44174c54c64
cepalle <epalle.cyrille@gmail.com>
2019-03-05 15:54:55 +0100

    add test

--- correction.sh
+++ correction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for D in `find ./test/corr/ -type f`
 do
-    echo -e "The file :\n"
+    echo "The file :\n"
     cat $D
     if [ $# -eq 1 ] ; then
         lein run $D "-f" &
@@ -9,8 +9,8 @@ do
         lein run $D &
     fi
     pid=$!
-    echo -e "\n\nTesting the file:\n"
+    echo "\n\nTesting the file:\n"
     wait $pid
-    sleep 10
-    echo -e "\n\n"
+    sleep 1
+    echo "\n\n"
 done
